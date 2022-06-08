cfbb2a4aa1dfaeacdfba1a8ee56d4780264e58e2
AlexOde <alexander.odegaard@gmail.com>
2022-06-08 21:19:45 +0200

    updated movies list, disabled cors, as it was not working. Need to fix

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
 const express = require('express')
 const app = express()
-const PORT = 8000
+const PORT = 8001
 
-const directorAndMovies = {
-    'kevin smith': {
-        'clerks': {
-            'name': 'Clerks',
-            'releaseYear': 1994,
-            'genre': 'comedy',
-            'personalRating': '1st place'
-        },
-        'mallrats': {
-            'name': 'Mallrats',
-            'releaseYear': 1994,
-            'genre': 'comedy',
-            'personalRating': '1st place'
-        },
-        'dogma': {
-            'name': 'Dogma',
-            'releaseYear': 1994,
-            'genre': 'comedy',
-            'personalRating': '1st place'
-        }
+
+const movies = {
+    'clerks': {
+        'name': 'Clerks',
+        'releaseYear': 1994,
     },
-    'thomas': {
-        'movie 1': {
-            'name': 'Movie 1',
-            'releaseYear': 1998,
-            'genre': 'drama',
-            'personalRating': '1st place'
-        },
+    'mallrats': {
+        'name': 'Mallrats',
+        'releaseYear': 1995,
     },
-    'unknown': {
-        'Search string': 'Empty'
+    'chasing amy': {
+        'name': 'Chasing Amy',
+        'releaseYear': 1997,
+    },
+    'dogma': {
+        'name': 'Dogma',
+        'releaseYear': 1999,
+    },
+    'jay and silent bob strike back': {
+        'name': 'Jay and Silent Bob Strike Back',
+        'releaseYear': 2001,
+    },
+    'jersey girl': {
+        'name': 'Jersey Girl',
+        'releaseYear': 2004,
+    },
+    'clerks ii': {
+        'name': 'Clerk II',
+        'releaseYear': 2006,
+    },
+    'zack and miri make a porno': {
+        'name': 'Zach and Miri Make a Porno',
+        'releaseYear': 2008,
     }
 }
 
@@ -40,10 +42,10 @@ app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:searchURL', (request, response)=>{
-    const searchString = request.params.searchURL.toLowerCase()
-    if (directorAndMovies[searchString]) {
-        response.json(directorAndMovies[searchString])
+app.get('/api/:searchurl', (request, response)=>{
+    const searchString = request.params.searchurl.toLowerCase()
+    if (movies[searchString]) {
+        response.json(movies[searchString])
     } else {
         response.json('unknown')
     }
